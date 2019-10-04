a598023aa5f14a20c5e811008d60766151e14ceb
Alex I. Nitu <10091929+novalex@users.noreply.github.com>
2019-10-04 17:22:33 +0300

    Create README.md

---
+++ README.md
@@ -0,0 +1,30 @@
+# Adminity
+Admin User Interface - HTML &amp; CSS template.
+
+## Not maintained
+This project is meant to serve as a time capsule to 2012, when it was first created.
+
+It was one of the first websites I made, so it doesn't use the best practices and hasn't been updated in many years, but it may still be useful to some people. For example, if your workplace still uses IE7, feel free to grab this, as IE7 compatibility was a requirement back when this was made :)
+
+### Plugins
+Huge thanks to the creators of jQuery and the awesome people contributing to the Open Source community!  
+[Flot](http://code.google.com/p/flot/)  
+[DataTables](http://datatables.net/)  
+[Full Calendar](http://arshaw.com/fullcalendar/)  
+[Syntax Highlighter](http://alexgorbatchev.com/SyntaxHighlighter/)  
+[Colorpicker](http://www.eyecon.ro/colorpicker/)  
+[elFinder File Manager](http://elfinder.org/)  
+[Lightbox](http://leandrovieira.com/projects/jquery/lightbox/)  
+[TipTip Tooltip](http://code.drewwilson.com/entry/tiptip-jquery-plugin)  
+[Sticky Notifications](http://thrivingkings.com/sticky/)  
+[iPhone style checkboxes](http://awardwinningfjords.com/2009/06/16/iphone-style-checkboxes.html)  
+[jQuery Validate](http://bassistance.de/jquery-plugins/jquery-plugin-validation/)  
+[NicEdit WYSIWYG Text Editor](http://nicedit.com/)  
+
+### Icons
+[FamFamFam Silk icon set](http://www.famfamfam.com/) (1000 icons)  
+[Fugue icon set](http://p.yusukekamiyamane.com/) (3400+ icons)  
+[Open Source icon set](http://www.greepit.com/open-source-icons-gcons/) (100 icons)  
+[Basic icon set](http://pixel-mixer.com/), not in sprite format, used for the table toolbar (7 icons)  
+[Glyphish icon set](http://glyphish.com/), not in sprite format, used for the navigation (27 icons, normal and active state + user avatar)  
+Background pattern - from [SubtlePatterns](subtlepatterns.com)  
--- php/connector.php
+++ php/connector.php
@@ -1,11 +1,11 @@
 <?php
 
-error_reporting(0); // Set E_ALL for debuging
+error_reporting( 0 ); // Set E_ALL for debuging
 
-include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderConnector.class.php';
-include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinder.class.php';
-include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeDriver.class.php';
-include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeLocalFileSystem.class.php';
+include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'elFinderConnector.class.php';
+include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'elFinder.class.php';
+include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'elFinderVolumeDriver.class.php';
+include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'elFinderVolumeLocalFileSystem.class.php';
 // Required for MySQL storage connector
 // include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeMySQL.class.php';
 
@@ -17,10 +17,10 @@ include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeLocalFileSyste
  * @param  string  $path  file path relative to volume root directory started with directory separator
  * @return bool
  **/
-function access($attr, $path, $data, $volume) {
-	return strpos(basename($path), '.') === 0   // if file/folder begins with '.' (dot)
-		? !($attr == 'read' || $attr == 'write')  // set read+write to false, other (locked+hidden) set to true
-		: ($attr == 'read' || $attr == 'write');  // else set read+write to true, locked+hidden to false
+function access( $attr, $path, $data, $volume ) {
+	return strpos( basename( $path ), '.' ) === 0   // if file/folder begins with '.' (dot)
+		? ! ( $attr == 'read' || $attr == 'write' )  // set read+write to false, other (locked+hidden) set to true
+		: ( $attr == 'read' || $attr == 'write' );  // else set read+write to true, locked+hidden to false
 }
 
 $opts = array(
@@ -29,18 +29,17 @@ $opts = array(
 		array(
 			'driver'        => 'LocalFileSystem',   // driver for accessing file system (REQUIRED)
 			'path'          => '../files/',         // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
+			'URL'           => dirname( $_SERVER['PHP_SELF'] ) . '/../files/', // URL to files (REQUIRED)
 			'accessControl' => 'access',             // disable and hide dot starting files (OPTIONAL)
-			'defaults'    => array(
-			       'read'        => true,
-			       'write'       => true,
-			       'rm'          => true
-		    )
-		)
-	)
+			'defaults'      => array(
+				'read'  => true,
+				'write' => true,
+				'rm'    => true,
+			),
+		),
+	),
 );
 
 // run elFinder
-$connector = new elFinderConnector(new elFinder($opts));
+$connector = new elFinderConnector( new elFinder( $opts ) );
 $connector->run();
-
